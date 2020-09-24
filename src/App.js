9364e6a96dbff857a519e9eb1a57afabb638c0b2
Chris Howard <choward@ownerfinancellc.com>
2020-09-24 14:32:02 -0400

    wired up correctly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import Jokes from './components/Jokes.js'
-import './App.css';
-
-export default function App(props) {
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Jokes from './components/Jokes';
+export default function App() {
+  const [jokes, setJokes] = useState([]);
+  const [formInputs, updateFormInputs] = useState({
+    author: '',
+    content: '',
+    title: ''
+  });
+  const getJokes = async ()=>{
+    try{
+      const response = await fetch('http://localhost:3000/jokes');
+      const data = await response.json();
+      setJokes(data)
+    }catch(error){
+      console.error(error)
+    }
+  }
+  useEffect(
+    ()=>{
+      (
+        async function (){
+           await getJokes();
+        }
+      )()
+    }, [])
+  const handleChange = (event) => {
+    const updatedFormInputs = Object.assign({}, formInputs, { [event.target.id]: event.target.value})
+    updateFormInputs(updatedFormInputs);
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        'http://localhost:3000/jokes',
+        formInputs
+      );
+      const createdJoke = response.data
+      await updateFormInputs({
+        author: '',
+        title: '',
+        content: ''
+      })
+      await setJokes([createdJoke, ...jokes])
+    }catch(error){
+      console.error(error)
+    }
+  }
   return (
     <div className="App">
-     <Jokes />
+        <div className="container">
+            <nav>
+              <h4>Post A Clean Joke</h4>
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="author">Write A Joke</label>
+                <input
+                  type="text"
+                  id="author"
+                  onChange={handleChange}
+                  value={formInputs.author}
+                  />
+                <label htmlFor="title">Ask A Joke</label>
+                <input
+                  type="text"
+                  id="title"
+                  onChange={handleChange}
+                  value={formInputs.title}
+                  />
+                <label htmlFor="content">Answer</label>
+                <input
+                  type="text"
+                  id="content"
+                  onChange={handleChange}
+                  value={formInputs.content}
+                  />
+                <input type="submit" className="submit" />
+              </form>
+            </nav>
+            <main>
+              <Jokes jokes={jokes}/>
+            </main>
+            <aside>
+            </aside>
+        </div>
+        <footer />
     </div>
   );
 }
-
--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,28 +1,21 @@
-import React, { useState, useEffect } from 'react';
-
+import React from 'react';
 export default function Jokes (props) {
-    const getJokes = async () => {
-        try {
-          const response = await fetch('http://localhost:3000/jokes')
-          const data = await response.json()
-          console.log(data)
-         } catch(error){
-           console.error(error)
-         }
-       } 
-        useEffect(()=>{
-         (
-         async function (){
-         await getJokes()
-         }
-         )()
-         },[])
-       
-         return(
-            <h1>Dad Jokes</h1>
-        )
-    };
-
+  return (
+    <div>
+      {
+        props.jokes.map( joke =>{
+          return (
+            <div key={joke.id} className="joke">
+              <h3>{joke.title}</h3>
+               <p> {joke.content}</p>
+               <small>{joke.author}</small>
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+}
 
 
 
